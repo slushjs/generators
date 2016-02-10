d0ba00af111c4a5bc5fd166056d2818d153d52bc
Joakim Carlstein <joakim.carlstein@gmail.com>
2016-02-10 08:17:11 +0100

    dist/ needs to be in repo (fixes #6)

--- dist-index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-var fs = require('fs');
-var join = require('path').join;
-
-var index = fs.readFileSync(join('src', 'app', 'index.html'), 'utf8');
-
-fs.writeFileSync(join('dist', 'index.html'), index.replace(/\.(js|css)/g, '.min.$1'), 'utf8');
